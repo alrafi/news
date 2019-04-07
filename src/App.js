c9092d812a80a1735a96189ea3074420fe05f197
hafisalrafi <13516034@std.stei.itb.ac.id>
2019-04-08 01:57:08 +0700

    add get category in main page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import News from './components/News';
 import './App.css';
 import Axios from 'axios';
+import GetCategory from './components/GetCategory';
 
 class App extends Component {
-  state = {
-    news: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      news:[]
+    };
+    this.getCategory = this.getCategory.bind(this);
   }
 
   componentDidMount() {
-    Axios.get('https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=1d5278d48fb74542922ba3c7dc6c7d56')
+    Axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=1d5278d48fb74542922ba3c7dc6c7d56')
     .then(res => {
       this.setState({
         news: res.data.articles
@@ -18,6 +23,15 @@ class App extends Component {
     })
   }
 
+  getCategory(value) {
+    let category = value;
+    let requestUrl = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=1d5278d48fb74542922ba3c7dc6c7d56&category=' + category;
+
+    Axios.get(requestUrl)
+    .then(res => {
+      this.setState({ news: res.data.articles })
+    });
+  }
 
   render() {
     return (
@@ -26,8 +40,9 @@ class App extends Component {
         <div className="container">
           <Route exact path="/" render={props => (
               <React.Fragment>
-                <News news={this.state.news}/>
-              </React.Fragment>
+                <GetCategory onSubmit={this.getCategory} />
+                <News news={this.state.news} onSubmit={this.getCategory} />
+              </React.Fragment>              
           )} />
         </div>
         </div>
