1263a09ac8f7ca533c9cd888909a3a31c70177ce
hafisalrafi <13516034@std.stei.itb.ac.id>
2019-04-07 23:55:34 +0700

    call newsapi test for title

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import News from './components/News';
 import './App.css';
+import Axios from 'axios';
 
 class App extends Component {
+  state = {
+    news: []
+  }
+
+  componentDidMount() {
+    Axios.get('https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=1d5278d48fb74542922ba3c7dc6c7d56')
+    .then(res => {
+      this.setState({
+        news: res.data.articles
+      })
+    })
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            The Alrafi Times
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+        <div className="container">
+          <Route exact path="/" render={props => (
+              <React.Fragment>
+                <News news={this.state.news}/>
+              </React.Fragment>
+          )} />
+        </div>
+        </div>
+      </Router>
     );
   }
 }
