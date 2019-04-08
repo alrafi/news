e13b45caacec172890ec01dc3e5321f9ac06c908
hafisalrafi <13516034@std.stei.itb.ac.id>
2019-04-08 14:46:30 +0700

    update news item

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,30 +1,46 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { withRouter } from "react-router-dom";
+import './Style.css';
 
 class NewsItem extends Component {
   getStyle = () => {
     return {
       background: '#f4f4f4',
       padding:'10px',
-      marginBottom:'10px'
+      marginBottom:'10px',
+      height: '250px'
     }
   }
   
   render() {
-    const { title, description } = this.props.thenews;
+    const { title, description, urlToImage } = this.props.thenews;
 
     return (
       <div style={this.getStyle()}>
         <div>
-          <p onClick={() => {this.props.changeSelectedIndex(this.props.index); this.props.history.push('/detail');}}>
+          <h2 className="title" onClick={() => {this.props.changeSelectedIndex(this.props.index); this.props.history.push('/detail');}}>
             <strong>{ title }</strong><br/>
+          </h2>
+          <p style={descStyle}>
+            <img src={urlToImage} style={imgStyle} alt="img" />
+            { description }
           </p>
-          { description }
+          
         </div>
       </div>
     )
   }
+}
+
+const imgStyle = {
+  width:'30%',
+  float:'left',
+  marginRight:'10px'
+}
+
+const descStyle = {
+  marginTop: '20px'
 }
 
 // PropTypes
