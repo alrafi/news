2f26bd3424fafc28e2551000603e4731f004c6ce
hafisalrafi <13516034@std.stei.itb.ac.id>
2019-04-08 14:45:08 +0700

    update detail page

--- src/components/pages/Detail.js
+++ src/components/pages/Detail.js
@@ -4,12 +4,36 @@ function Detail(props) {
   console.log(props)
   return (
     <>
-      <h1>Detail News</h1>
-      <p>
-        { props.news.content }
-      </p>
+      <div style={contentStyle}>
+        <h1 style={titleStyle}>{ props.news.title }</h1>
+        <p>
+          <img src={props.news.urlToImage} alt="img" style={imgStyle} /> <br/>
+          Author: {props.news.author} <br/>
+          Published: {props.news.publishedAt} <br/><br/>
+          { props.news.content } <br/><br/>
+          <strong><a href={props.news.url}>Read More</a></strong>
+        </p>
+      </div>
     </>
   )
+}
+
+const imgStyle = {
+  marginTop: '20px',
+  width: "50%",
+  display: 'block',
+  marginLeft: 'auto',
+  marginRight: 'auto'
+}
+
+const contentStyle = {
+  background: '#f6f6f6',
+  paddingLeft: '10px',
+  paddingRight: '10px'
+}
+
+const titleStyle = {
+  paddingTop: '20px'
 }
 
 export default Detail
